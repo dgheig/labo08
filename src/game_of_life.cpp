eaa0b312a911b954841dbb5b90f1bda4d9ddd6ca
heigvdgachetje <jean.gachet@heig-vd.ch>
2019-11-26 13:52:17 +0100

    ajout de la fonction nbOfNeighbours

--- src/game_of_life.cpp
+++ src/game_of_life.cpp
@@ -12,7 +12,21 @@ void displayGame(bool tab[HEIGHT][WIDTH])
    
 }
 
-unsigned neighbour(size_t x, size_t y)
+
+unsigned nbOfNeighbours(int line, int column, bool tab[HEIGHT][WIDTH])
 {
-   
+   unsigned neighbours = 0;
+   for (int l = -1; l <= 1; ++l)       //
+   {                                   //for all neighbours
+      for (int c = -1; c <= 1; ++c)    //
+      {
+         if ( !(l == 0 && c == 0 ) &&                    //exluding self
+              line + l >= 0 && column + c >= 0 &&        //excluding underflow
+              line + l < HEIGHT && column + c < WIDTH )  //excluding overflow
+         {
+            neighbours += (unsigned)tab[line - l][column - c];
+         }
+      }
+   }
+   return neighbours;
 }
