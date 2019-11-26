a22c0dde9d6f61ef1d8b13b66b5199fd3174eccd
heigvdgachetje <jean.gachet@heig-vd.ch>
2019-11-26 13:23:33 +0100

    renommage de nextGen, ajout de computeMultipleGens et displayGame

--- src/game_of_life.cpp
+++ src/game_of_life.cpp
@@ -1,7 +1,18 @@
 #include "game_of_life.h"
 
+using namespace std;
 
-void nextGen(bool tab[HEIGHT][WIDTH])
+void computeNextGen(bool tab[HEIGHT][WIDTH])
 {
 
+}
+
+void displayGame(bool tab[HEIGHT][WIDTH])
+{
+   
+}
+
+unsigned neighbour(size_t x, size_t y)
+{
+   
 }
--- src/game_of_life.h
+++ src/game_of_life.h
@@ -1,9 +1,15 @@
 #ifndef GAME_OF_LIFE_H
 #define GAME_OF_LIFE_H
 
-#define HEIGHT 30
-#define WIDTH 30
+#define HEIGHT 8
+#define WIDTH 8
+#define ALIVE true
+#define DEAD false
 
-void nextGen(bool tab[HEIGHT][WIDTH]);
+void computeNextGen(bool tab[HEIGHT][WIDTH]);
+
+void computeMultipleGens(unsigned n, bool tab[HEIGHT][WIDTH]);
+
+void displayGame(bool tab[HEIGHT][WIDTH]);
 
 #endif // GAME_OF_LIFE_H
