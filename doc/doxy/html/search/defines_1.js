3f45b9bb2ae28594abe2b6a9e19a5e5a0dc58ecc
Gallay David <david.gallay@heig-vd.ch>
2019-11-29 21:57:55 +0100

    run labo08 build with workflow

--- doc/doxy/html/search/defines_1.js
+++ doc/doxy/html/search/defines_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['dead',['DEAD',['../game__of__life_8h.html#a3c8793c7acb4598d2ebcd8288f29ee69',1,'game_of_life.h']]],
-  ['dead_5fchar',['DEAD_CHAR',['../game__of__life_8h.html#a1e50cf7b88288775430bb0f360db5bf6',1,'game_of_life.h']]],
-  ['debug',['DEBUG',['../game__of__life_8h.html#ad72dbcf6d0153db1b8d8a58001feed83',1,'game_of_life.h']]]
+  ['dead_5fchar',['DEAD_CHAR',['../game__of__life_8h.html#a1e50cf7b88288775430bb0f360db5bf6',1,'game_of_life.h']]]
 ];
